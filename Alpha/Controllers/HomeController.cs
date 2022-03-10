0520a89d30c01602178d1c86082fa9b17b20c272
felicia-kho <63596234+felicia-kho@users.noreply.github.com>
2022-03-10 13:21:29 +0800

    Add project files.

---
+++ Alpha/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpha.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult UsingNeutralisationReaction()
+        {
+            return View();
+        }
+
+        public ActionResult WhatAreAlkalis()
+        {
+            return View();
+        }
+
+        public ActionResult WhatAreAcid()
+        {
+            return View();
+        }
+
+        public ActionResult DetectingAcidAndAlkalisUsingIndicators()
+        {
+            return View();
+        }
+
+        public ActionResult ThePhScale()
+        {
+            return View();
+        }
+
+        public ActionResult Neutralisation()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QuizUsingNeutralisationReaction()
+        {
+            return View();
+        }
+
+        public ActionResult QuizForPhScale()
+        {
+            return View();
+        }
+        public ActionResult QuizNeutralisation()
+        {
+            return View();
+        }
+        public ActionResult QuizForWhatAreAcids()
+        {
+            return View();
+        }
+        public ActionResult QuizForWhatAreAlkalis()
+        {
+            return View();
+        }
+
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+
+    }
+}
